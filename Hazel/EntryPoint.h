387e49d0056c3bce71bfb35f5436461feca26fca
poiuy32112 <why10242018@outlook.com>
2024-12-04 18:13:13 +0800

    新增和修改

--- Hazel/EntryPoint.h
+++ Hazel/EntryPoint.h
@@ -1,14 +1,16 @@
 #pragma once
-#include <iostream>
 
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
 
-	//ÐÂ·ÖÖ§
-	std::cout << "adsfadsf";
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
